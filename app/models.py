7d0ca6d39bb073eab6e7002af4eb6b5bcde82fd3
Daniel Corcoran <danielc.92@outlook.com>
2019-05-13 18:26:32 +1000

    Added models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Hobby(models.Model):
+
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Person(models.Model):
+    
+    name = models.CharField(max_length=100)
+    hobbies = models.ManyToManyField(Hobby)
+
+    def __str__(self):
+        return self.name
+
