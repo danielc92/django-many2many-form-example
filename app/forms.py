3f00aa14f049fb3aae021eb2b674e89c98f91de9
Daniel Corcoran <danielc.92@outlook.com>
2019-05-13 18:45:38 +1000

    Made database

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,14 @@
-from models import Person, Hobby
+from .models import Person, Hobby
 from django import forms
 
 
-class PersonForm(forms.Form):
+class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ['name', 'height', 'hobbies']
 
 
-class HobbyForm(forms.Form):
+class HobbyForm(forms.ModelForm):
     class Meta:
         model = Hobby
         fields = ['name']
--- app/views.py
+++ app/views.py
@@ -14,11 +14,34 @@ def create_hobby(request):
             return redirect('success')
     else:
 
-        form = HobbyForm(request.GET)
+        form = HobbyForm()
 
-    context = {'form': form, 'title':'create a hobby!'}
+    context = {'form': form, 'title':'Create a hobby!'}
 
-    return render(request, 'create', context)
+    print(form.as_p)
+
+    return render(request, 'create.html', context)
+
+
+def create_person(request):
+
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+
+            return redirect('success')
+    else:
+
+        form = PersonForm()
+
+    context = {'form': form, 'title':'Add a person!'}
+
+    return render(request, 'create.html', context)
+
+
 
 def success(request):
 
